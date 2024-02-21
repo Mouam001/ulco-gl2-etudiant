fc1f44d8d9cfaed847f113f675abf5eee8fac929
TODO <TODO>
2024-02-21 17:26:39 +0100

    Logger sans les heures

--- tp-ffi/py-logger/cpp/Logger.cpp
+++ tp-ffi/py-logger/cpp/Logger.cpp
@@ -6,16 +6,31 @@
 #include <sstream>
 
 void Logger::addItem(Level l, const std::string & m) {
-  // TODO implement Logger::addItem
+  //implement Logger::addItem
+  _items.emplace_back(l, m);
 }
+
 
 std::string Logger::reportByAdded() const {
-  // TODO Logger::reportByAdded
-  return "TODO Logger::reportByAdded";
+   std::stringstream ss;
+    for (const auto & item : _items) {
+        ss << "[" << static_cast<char>(std::get<0>(item)) << "] " << std::get<1>(item) << std::endl;
+    }
+    return ss.str();
 }
 
+
 std::string Logger::reportByLevel() const {
-  // TODO Logger::reportByLevel
-  return "TODO Logger::reportByLevel";
+    std::map<Level, std::vector<std::string>> groupedItems;
+    for (const auto & item : _items) {
+        groupedItems[std::get<0>(item)].push_back(std::get<1>(item));
+    }
+    std::stringstream ss;
+    for (const auto & pair : groupedItems) {
+        for (const auto & message : pair.second) {
+            ss << "[" << static_cast<char>(pair.first) << "] " << message << std::endl;
+        }
+    }
+    return ss.str();
 }
 
--- tp-ffi/py-logger/scripts/main1.py
+++ tp-ffi/py-logger/scripts/main1.py
@@ -1,11 +1,22 @@
-
+import Logger
 if __name__ == '__main__':
 
     print('TODO')
 
-    # TODO create a logger and add some items
+    #create a logger and add some items
+    log = Logger.Logger()
+    log.add_item(Logger.Level.Info, "une autre info")
+    log.add_item(Logger.Level.Info, "une info")
+    log.add_item(Logger.Level.Error, "une Erreur")
+    log.add_item(Logger.Level.Warning, "un avertissement")
+    
+    
 
-    # TODO report_by_added
-
-    # TODO report_by_level
+    #report_by_added
+    print("**** report_by_added ****")
+    print(log.report_by_added())
+    #report_by_level
+    print("**** report_by_level ****")
+    print(log.report_by_level())
+    
 

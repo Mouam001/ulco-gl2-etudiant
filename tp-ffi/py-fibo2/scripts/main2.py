40ee226e612435e2626e5848776fe13850267612
TODO <TODO>
2024-02-21 16:29:06 +0100

    "fibo en graphe main2"

--- tp-ffi/py-fibo2/scripts/main2.py
+++ tp-ffi/py-fibo2/scripts/main2.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
+import myfibo
 
 
 def test_func(x: int):
@@ -10,12 +11,12 @@ def test_func(x: int):
 
 if __name__ == '__main__':
 
-    xs = np.arange(-10, 11, 1)
-    ys = np.vectorize(test_func)(xs)
+    xs = np.arange(11)
+    ys = np.vectorize(myfibo.fibo_naive)(xs)
 
     fig, ax = plt.subplots()
     ax.plot(xs, ys)
-    ax.set(xlabel='i', ylabel='test_func(i)')
+    ax.set(xlabel='i', ylabel='fibo_naive(i)')
     ax.grid()
 
     fig.savefig("output.png")

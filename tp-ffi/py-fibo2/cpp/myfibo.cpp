db273e8ece3418d09d82463ac4e84307c6dbde59
TODO <TODO>
2024-02-21 16:14:12 +0100

    main1 fibo naive et iterative C++

--- tp-ffi/py-fibo2/cpp/myfibo.cpp
+++ tp-ffi/py-fibo2/cpp/myfibo.cpp
@@ -5,16 +5,28 @@ int fiboNaive(int n) {
     return n < 2 ? n : fiboNaive(n-1) + fiboNaive(n-2);
 }
 
-// TODO implement fiboIterative
-
+//implement fiboIterative
+int fiboIterative(int n){
+    assert (n >= 0);
+    if (n < 2){
+        return n;
+    }
+    int fibo_prev = 0;
+    int fibo_recc = 1;
+    for (int i = 2; i < n; i++){
+      int fibo_next = fibo_prev + fibo_recc;
+        fibo_prev = fibo_recc;
+        fibo_recc = fibo_next;
+    }
+    return fibo_recc;
+}
 
 #include <pybind11/pybind11.h>
-
 PYBIND11_MODULE(myfibo, m) {
 
-    // TODO export fiboNaive (as fibo_naive)
-
-    // TODO export fiboIterative (as fibo_iterative)
-
+    //export fiboNaive (as fibo_naive)
+     m.def("fibo_naive", &fiboNaive);
+     //export fiboIterative (as fibo_iterative)
+     m.def("fibo_itarative", &fiboIterative);
 }
 

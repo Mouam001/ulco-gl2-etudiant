6a6040b51c8f4f138dc3b5cf4023486a4653b8ab
TODO <TODO>
2024-04-18 15:59:42 +0200

    dessin du jeu (close #8)

--- tp-workflow/scripts/tictactoe_gui.py
+++ tp-workflow/scripts/tictactoe_gui.py
@@ -40,22 +40,43 @@ class Gui(Gtk.Window):
         hbox.pack_start(button2, True, True, 0)
 
         #create game (from the C++ module)
-       # self.jeu = tictactoe.Jeu
+        self.jeu = tictactoe.Jeu
        # self.update_status_label()
 
     def on_draw(self, widget, context):
 
-        # TODO on_draw
+        #on_draw
         
-        width = widget.get_allocated_width()
-        height = widget.get_allocated_height()
+        allocation = widget.get_allocation()
+        width = allocation.width
+        height = allocation.height
 
+    # Calculer la taille de chaque case
+        cell_width = width / 3
+        cell_height = height / 3
+
+    # Dessiner le fond noir
         context.set_source_rgb(0, 0, 0)
-        context.rectangle(0, 0, 100, 100)
-        context.fill()
+        context.paint()
 
-        context.set_source_rgb(0.5, 0.5, 0.5)
-        context.set_line_width(3)
+    # Dessiner les lignes de séparation blanches
+        context.set_source_rgb(1, 1, 1)
+        context.set_line_width(2)
+
+    # Dessiner les lignes horizontales
+        for i in range(1, 3):
+            context.move_to(0, i * cell_height)
+            context.line_to(width, i * cell_height)
+            context.stroke()
+
+        # Dessiner les lignes verticales
+        for i in range(1, 3):
+            context.move_to(i * cell_width, 0)
+            context.line_to(i * cell_width, height)
+            context.stroke()
+
+
+        # Dessiner la diagonale secondaire
         context.move_to(width, 0)
         context.line_to(0, height)
         context.stroke()
@@ -65,11 +86,14 @@ class Gui(Gtk.Window):
         # TODO on_area_button_press
         if event.button == 1:
             print('TODO on_area_button_press')
+            self.jeu.jouer(0, 0)
+            self.drawingarea.queue_draw()
             
 
     def on_button1_clicked(self, widget):
         # TODO on_button1_clicked
         print('TODO on_button1_clicked')
+        self.jeu = tictactoe.Jeu()
         self.drawingarea.queue_draw()
 
     def on_button2_clicked(self, widget):
